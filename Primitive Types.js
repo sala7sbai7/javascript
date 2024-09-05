129d5786ae57e445b3d8b924c8393c52d4cb2019
sala7sbai7 <sala7.sbai7@hotmail.com>
2024-09-05 07:48:26 +0300

    Add files via upload

---
+++ Primitive Types.js
@@ -0,0 +1,16 @@
+let myName = "Salah";
+console.log(myName);
+
+let num1 = 10;
+let num2 = 20;
+console.log (num1 + num2);
+
+let anything = true;
+console.log(typeof anything)
+
+let empty;
+console.log (empty);
+
+let empty1 = null
+console.log (empty1);
+

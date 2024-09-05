7b5f36f258e50893e30c799ca49b83cbf5a1e779
sala7sbai7 <sala7.sbai7@hotmail.com>
2024-09-05 11:38:21 +0300

    Add files via upload

---
+++ 11.if-else statement.js
@@ -0,0 +1,7 @@
+// let movieRating = prompt ("whats your rating");
+let movieRating = 9;
+if (movieRating > 7) {
+    console.log ("highly rated movie")
+} else {
+    console.log ("this movie has room for improvment")
+};
